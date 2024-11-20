6af6e54f62cc6a811b595a36bfd77a2cab1d9337
N1CKZ3V <nico_03_sebas@hotmail.com>
2024-11-20 18:01:11 -0500

    Creación exitosa

--- FunctionProject/Fibonacci/index.js
+++ FunctionProject/Fibonacci/index.js
@@ -1,3 +1,4 @@
+/*
 var bigInt = require("big-integer");
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
@@ -25,3 +26,57 @@ module.exports = async function (context, req) {
         body: answer.toString()
     };
 }
+*/
+
+var bigInt = require("big-integer");
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    let nth = req.body.nth;
+
+    if (nth < 0) {
+        throw 'El valor de "nth" debe ser un número entero no negativo.';
+    }
+
+    var memo = {};
+
+    function fibonacci(n) {
+        if (n === 0) {
+            return bigInt.zero;
+        } else if (n === 1) {
+            return bigInt.one;
+        } else if (memo[n]) {
+            return memo[n];
+        } else {
+            return (memo[n] = fibonacciHelper(n));
+        }
+    }
+
+    function fibonacciHelper(n) {
+        let a = bigInt.zero;
+        let b = bigInt.one;
+
+        for (let i = 0; i < n - 1; i++) {
+            let temp = a;
+            a = b;
+            b = temp.add(b);
+        }
+
+        return a;
+    }
+
+    try {
+        let answer = fibonacci(nth);
+
+        context.res = {
+            body: answer.toString()
+        };
+    } catch (error) {
+        context.log.error(error);
+        context.res = {
+            status: 500,
+            body: "Error en la ejecución de la función."
+        };
+    }
+};
